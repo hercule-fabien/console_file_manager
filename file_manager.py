64f06649101f2b21d325994a959979a761a4b6cf
hercule-fabien <vvg.doc@yandex.ru>
2022-10-06 22:23:00 +0300

    added "delete_folder" function

--- file_manager.py
+++ file_manager.py
@@ -8,6 +8,26 @@ def create_folder(folder_name):
     :param folder_name: str
     :return: None
     """
-    if not os.path.exists(folder_name):
-        os.mkdir(folder_name)
+    while True:
+        if os.path.exists(folder_name):
+            print("The folder with this name already exists.")
+            folder_name = input("Please enter the other name: ")
+        if not os.path.exists(folder_name):
+            os.mkdir(folder_name)
+            print("Folder was created")
+            break
 
+
+def delete_folder():
+    keep_asking = True
+    while keep_asking:
+        f_name = input("Enter the folder/file name to delete: ")
+        if os.path.exists(f_name):
+            os.rmdir(f_name)
+            print("Folder/file deleted")
+        else:
+            print("This folder doesn't exist")
+
+        choice = input("Delete another? (y/n): ")
+        if choice.lower() == "n":
+            keep_asking = False
