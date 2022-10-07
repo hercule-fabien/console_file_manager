0ce73eb7d0415acbd581acb49788c2de96d48fdb
hercule-fabien <vvg.doc@yandex.ru>
2022-10-07 10:36:06 +0300

    added menu items #3,4

--- file_manager.py
+++ file_manager.py
@@ -1,5 +1,13 @@
 import os
+import shutil
 import sys
+
+
+def choice(question):
+    result = input(question + "? (y/n): ")
+    if result.lower() == "n":
+        return False
+    return True
 
 
 def create_folder(folder_name):
@@ -19,15 +27,82 @@ def create_folder(folder_name):
 
 
 def delete_folder():
+    """
+    Deletes a folder or a file of user choice
+    :return: None
+    """
     keep_asking = True
     while keep_asking:
         f_name = input("Enter the folder/file name to delete: ")
-        if os.path.exists(f_name):
-            os.rmdir(f_name)
-            print("Folder/file deleted")
+        if os.path.isdir(f_name):
+            if os.path.exists(f_name):
+                os.rmdir(f_name)
+                print("Folder deleted")
+            else:
+                print("This folder doesn't exist")
+        elif os.path.isfile(f_name):
+            if os.path.exists(f_name):
+                os.remove(f_name)
+                print("The file deleted")
+            else:
+                print("This file doesn't exist")
         else:
-            print("This folder doesn't exist")
+            print("This doesn't exist, try another")
 
         choice = input("Delete another? (y/n): ")
         if choice.lower() == "n":
             keep_asking = False
+
+
+# Opted out this function as os.mknod requires root privileges on
+# MacOS, therefore may not work for some users
+# def create_file():
+#     f_name = input("Enter the file name to create: ")
+#     if not os.path.exists(f_name):
+#         os.mknod(f_name)
+#         print("The file created")
+#     else:
+#         print("The file already exists")
+
+
+def create_file():
+    keep_asking = True
+    create_another = True
+    while create_another:
+        while keep_asking:
+            f_name = input("Enter the file name to create: ")
+            if not os.path.exists(f_name):
+                open(f_name, 'a').close()
+                print("The file created")
+                break
+            else:
+                print("The file already exists")
+
+            keep_asking = choice("Try another name")
+        create_another = choice("Create another file")
+
+
+def copy_file():
+    keep_asking = True
+    copy_another = True
+    while copy_another:
+        while keep_asking:
+            f_name = input("Enter the folder/file name to copy: ")
+            copy_name = input("Enter the name for copy: ")
+            if os.path.exists(f_name):
+                if not os.path.exists(copy_name):
+                    if os.path.isfile(f_name):
+                        shutil.copy(f_name, copy_name)
+                        print("The file copied")
+                        break
+                    elif os.path.isdir(f_name):
+                        shutil.copytree(f_name, copy_name)
+                        print("Success!")
+                        break
+                else:
+                    print("The name already exists")
+            else:
+                print("The file with this name doesn't exist")
+            keep_asking = choice("Try another name")
+
+        copy_another = choice("Copy another file")
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from bank_account import manage_bank_account
-from file_manager import create_folder, delete_folder
+from file_manager import create_folder, delete_folder, create_file, copy_file
 
 
 def main():
@@ -8,30 +8,35 @@ def main():
     print("Please pick your desired action from the menu below: ")
     print("""
         1. Create a folder.
-        2. Delete (file/folder).
-        3. Copy (file/folder).
-        4. Check the files in the working directory.
-        5. Display folders only.
-        6. Display files only.
-        7. Check OS type.
-        8. Credits.
-        9. Play the Victory game.
-        10. Check you bank account status.
-        11. Change working directory.
-        12. Quit.
+        2. Create a file.
+        3. Delete (file/folder).
+        4. Copy (file/folder).
+        5. Check the files in the working directory.
+        6. Display folders only.
+        7. Display files only.
+        8. Check OS type.
+        9. Show credits.
+        10. Play the Victory game.
+        11. Check you bank account status.
+        12. Change working directory.
+        13. Quit.
     """)
 
     while True:
         user_choice = int(input("Choose menu item to continue: "))
-        if user_choice in range(1, 13):
-            if user_choice == 10:
-                manage_bank_account()
-            elif user_choice == 1:
+        if user_choice in range(1, 14):
+            if user_choice == 1:
                 f_name = input("Enter the folder name: ")
                 create_folder(f_name)
             elif user_choice == 2:
+                create_file()
+            elif user_choice == 3:
                 delete_folder()
-            elif user_choice == 12:
+            elif user_choice == 4:
+                copy_file()
+            elif user_choice == 11:
+                manage_bank_account()
+            elif user_choice == 13:
                 print("Good bye!")
                 break
         else:
